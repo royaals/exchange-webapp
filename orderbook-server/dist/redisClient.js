26a359fdd5c57d66850b639dc2a54924a0f11ded
Royal <royal.21ai040@sode-edu.in>
2024-08-24 13:30:13 +0530

    delte node

--- orderbook-server/dist/redisClient.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = require("redis");
-const client = (0, redis_1.createClient)();
-client.connect();
-exports.default = client;
